e622b90330405df450796970cf7a43793f617e8a
wangshiyu <15134966191@163.com>
2023-03-13 23:57:56 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/tailwind.css";
+import "@icon-park/vue-next/styles/index.css";
+
+const app = createApp(App);
+
+app.use(router).mount("#app");
